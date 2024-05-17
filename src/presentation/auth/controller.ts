8c9c017e29a4aa4688272a6840d31a975491f6af
CristiAndLook <krhistian1902@gmail.com>
2024-05-16 23:36:48 -0500

    Auth Service

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,20 +1,33 @@
 import { Request, Response } from 'express';
+import { RegisterUserDto } from '../../domain';
+import { AuthService } from '../services/auth.service';
 
 export class AuthController {
 
     //DI
-    constructor() {
+    constructor(
+        public readonly authService: AuthService,
+    ) {
     }
 
     loginUser = (req: Request, res: Response) => {
+
         res.json('user login');
     }
 
     registerUser = (req: Request, res: Response) => {
-        res.json('user register');
+
+        const [error, registerDto] = RegisterUserDto.create(req.body);
+
+        if (error) return res.status(400).json(error);
+
+        this.authService.registerUser(registerDto!)
+            .then((user) => res.json(user))
+
     }
 
     validateEmail = (req: Request, res: Response) => {
+
         res.json('validate email');
     }
 }
